9284792eaf752a60024e132656040d77f8635636
FantasyMu <king756510@gmail.com>
2020-11-07 17:26:01 +0800

    first commit

---
+++ mysite/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+class PlayList(models.Model):
+    name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.name
+
+class Video(models.Model):
+    plist = models.ForeignKey(PlayList, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    vid = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
